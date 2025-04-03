278cfa6f7c7126d91dca284369d4b4d6973c5ce3
peterdinis <petter1@pobox.sk>
2025-04-03 18:59:30 +0200

    feat: Updating Editor WIP

--- app/_components/canvas/CanvasElement.tsx
+++ app/_components/canvas/CanvasElement.tsx
@@ -1,122 +1,107 @@
 "use client";
 
-import type {
-	PDFElement,
-	PencilDrawingElement,
-	ShapeElement,
-	TableElement,
-	TextElement,
-	Tool,
-} from "@/types";
-import Image from "next/image";
-import type React from "react";
-import { TextEditor } from "../editor/TextEditor";
-import { PencilTool } from "../tools/PencilTool";
-import { ShapeTool } from "../tools/ShapeTool";
-import { TableTool } from "../tools/TableTool";
+import { PDFElement, Tool, TextElement, ShapeElement, TableElement, PencilDrawingElement } from '@/types';
+import React from 'react';
+import { TextEditor } from '../editor/TextEditor';
+import { PencilTool } from '../tools/PencilTool';
+import { ShapeTool } from '../tools/ShapeTool';
+import { TableTool } from '../tools/TableTool';
 
 interface CanvasElementProps {
-	element: PDFElement;
-	isSelected: boolean;
-	onMouseDown: (e: React.MouseEvent) => void;
-	onUpdate: (element: PDFElement) => void;
-	activeTool: Tool;
-	onAddElement: (element: PDFElement) => void;
-	isEditing: boolean;
-	setIsEditing: (isEditing: boolean) => void;
+  element: PDFElement;
+  isSelected: boolean;
+  onMouseDown: (e: React.MouseEvent) => void;
+  onUpdate: (element: PDFElement) => void;
+  activeTool: Tool;
+  onAddElement: (element: PDFElement) => void;
+  isEditing: boolean;
+  setIsEditing: (isEditing: boolean) => void;
 }
 
 export const CanvasElement: React.FC<CanvasElementProps> = ({
-	element,
-	isSelected,
-	onMouseDown,
-	onUpdate,
-	activeTool,
-	onAddElement,
-	isEditing,
-	setIsEditing,
+  element,
+  isSelected,
+  onMouseDown,
+  onUpdate,
+  activeTool,
+  onAddElement,
+  isEditing,
+  setIsEditing
 }) => {
-	// Determine which component to render based on element type
-	switch (element.type) {
-		case "text":
-			return (
-				<TextEditor
-					element={element as TextElement}
-					isSelected={isSelected}
-					onMouseDown={onMouseDown}
-					onUpdate={onUpdate}
-					isEditing={isEditing}
-					setIsEditing={setIsEditing}
-				/>
-			);
-		case "image":
-			return (
-				<ImageElement
-					element={element}
-					isSelected={isSelected}
-					onMouseDown={onMouseDown}
-				/>
-			);
-		case "shape":
-			return (
-				<ShapeTool
-					element={element as ShapeElement}
-					isSelected={isSelected}
-					onMouseDown={onMouseDown}
-					onUpdate={onUpdate}
-				/>
-			);
-		case "table":
-			return (
-				<TableTool
-					element={element as TableElement}
-					isSelected={isSelected}
-					onMouseDown={onMouseDown}
-					onUpdate={onUpdate}
-				/>
-			);
-		case "pencil":
-			return (
-				<PencilTool
-					element={element as PencilDrawingElement}
-					isSelected={isSelected}
-					onMouseDown={onMouseDown}
-				/>
-			);
-		default:
-			return null;
-	}
+  // Determine which component to render based on element type
+  switch (element.type) {
+    case 'text':
+      return (
+        <TextEditor 
+          element={element as TextElement}
+          isSelected={isSelected}
+          onMouseDown={onMouseDown}
+          onUpdate={onUpdate}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+        />
+      );
+    case 'image':
+      return <ImageElement element={element} isSelected={isSelected} onMouseDown={onMouseDown} />;
+    case 'shape':
+      return (
+        <ShapeTool
+          element={element as ShapeElement}
+          isSelected={isSelected}
+          onMouseDown={onMouseDown}
+          onUpdate={onUpdate}
+        />
+      );
+    case 'table':
+      return (
+        <TableTool
+          element={element as TableElement}
+          isSelected={isSelected}
+          onMouseDown={onMouseDown}
+          onUpdate={onUpdate}
+        />
+      );
+    case 'pencil':
+      return (
+        <PencilTool
+          element={element as PencilDrawingElement}
+          isSelected={isSelected}
+          onMouseDown={onMouseDown}
+        />
+      );
+    default:
+      return null;
+  }
 };
 
 // A separate component for image elements
 const ImageElement: React.FC<{
-	element: PDFElement;
-	isSelected: boolean;
-	onMouseDown: (e: React.MouseEvent) => void;
+  element: PDFElement;
+  isSelected: boolean;
+  onMouseDown: (e: React.MouseEvent) => void;
 }> = ({ element, isSelected, onMouseDown }) => {
-	if (element.type !== "image") return null;
-
-	// Check if the image is an SVG (either by extension or by data URI type)
-	const isSvg =
-		element.src.toLowerCase().endsWith(".svg") ||
-		element.src.toLowerCase().includes("image/svg+xml");
-
-	return (
-		<div
-			className={`absolute cursor-move ${isSelected ? "ring-2 ring-editor-primary ring-offset-2" : ""}`}
-			style={{
-				left: `${element.x}px`,
-				top: `${element.y}px`,
-				width: `${element.width}px`,
-				height: `${element.height}px`,
-			}}
-			onMouseDown={onMouseDown}
-		>
-			<Image
-				src={element.src}
-				alt="PDF element"
-				className="w-full h-full object-contain"
-			/>
-		</div>
-	);
+  if (element.type !== 'image') return null;
+  
+  // Check if the image is an SVG (either by extension or by data URI type)
+  const isSvg = element.src.toLowerCase().endsWith('.svg') || 
+                element.src.toLowerCase().includes('image/svg+xml');
+  
+  return (
+    <div
+      className={`absolute cursor-move ${isSelected ? "ring-2 ring-editor-primary ring-offset-2" : ""}`}
+      style={{
+        left: `${element.x}px`,
+        top: `${element.y}px`,
+        width: `${element.width}px`,
+        height: `${element.height}px`,
+      }}
+      onMouseDown={onMouseDown}
+    >
+      <img 
+        src={element.src} 
+        alt="PDF element" 
+        className="w-full h-full object-contain"
+      />
+    </div>
+  );
 };
